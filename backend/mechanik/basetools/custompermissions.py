2541a687c68db17eb9aca0940f3e971abdafe811
Jakub K <kuba1095@gmail.com>
2022-11-14 22:49:01 +0100

    restructure v2

--- backend/mechanik/basetools/custompermissions.py
+++ backend/mechanik/basetools/custompermissions.py
@@ -3,10 +3,9 @@ from rest_framework import permissions
 class IsMechanicPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        user_role = request.user.userinfo
-        print(
-            user_role.__dict__
-        )
+        user = request.user
+
+        user_role = user.userinfo.role
         if user_role == "M":
             return True
         return False
