6e91399f8149d94baffd62ee4cea339023ada741
Jakub Kościelny <kuba1095@gmail.com>
2022-12-27 23:36:47 +0100

    fill needed endpoints for visit

--- backend/mechanik/visit/urls.py
+++ backend/mechanik/visit/urls.py
@@ -11,9 +11,19 @@ router.register(
     'service'
 )
 router.register(
+    r'serviceClient',
+    views.ServiceClientViewSet,
+    'serviceClient'
+)
+router.register(
     r'servicename',
     views.ServiceNameViewSet,
     'servicename'
+)
+router.register(
+    r'servicenameClient',
+    views.ServiceNameClientViewSet,
+    'servicenameClient'
 )
 router.register(
     r'repair',
@@ -26,13 +36,47 @@ router.register(
     'visitreason'
 )
 router.register(
+    r'visitreasonClient',
+    views.VisitReasonClientViewSet,
+    'visitreasonClient'
+)
+router.register(
+    r'visitdescription',
+    views.VisitDescriptionViewSet,
+    'visitdescription'
+)
+router.register(
+    r'visitdescriptionClient',
+    views.VisitDescriptionClientViewSet,
+    'visitdescriptionClient'
+)
+router.register(
     r'visit',
     views.VisitViewSet,
     'visit'
 )
+router.register(
+    r'visitClient',
+    views.VisitClientViewSet,
+    'visitClient'
+)
+router.register(
+    r'carplace',
+    views.CarPlaceViewSet,
+    'carplace'
+)
+router.register(
+    r'carplaceClient',
+    views.CarPlaceClientViewSet,
+    'carplaceClient'
+)
+router.register(
+    r'yourreparingcars',
+    views.YourReparingCars,
+    'yourreparingcars'
+)
 
 
 urlpatterns = [
-    path('yourreparingcars/',views.YourReparingCars.as_view()),
     path('',include(router.urls))
 ]
--- backend/mechanik/visit/views.py
+++ backend/mechanik/visit/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from basetools.schema import MyOwnSchema
 from basetools.custompermissions import IsMechanicPermission
-
+from rest_framework import status,mixins
 
 from visit.models import Service, ServiceName, Repair, VisitReason, VisitDescription,\
     Visit, CarPlace
@@ -19,63 +19,91 @@ class ServiceViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
 
+class ServiceClientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated,IsMechanicPermission]
+
+##############
 class ServiceNameViewSet(viewsets.ModelViewSet):
     queryset = ServiceName.objects.all()
     serializer_class = ServiceNameSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
 
+class ServiceNameClientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = ServiceName.objects.all()
+    serializer_class = ServiceNameSerializer
+    permission_classes = [IsAuthenticated,IsMechanicPermission]
+########
 class RepairViewSet(viewsets.ModelViewSet):
     queryset = Repair.objects.all()
     serializer_class = RepairSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
-
+########
 class VisitReasonViewSet(viewsets.ModelViewSet):
     queryset = VisitReason.objects.all()
     serializer_class = VisitReasonSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
 
+class VisitReasonClientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = VisitReason.objects.all()
+    serializer_class = VisitReasonSerializer
+    permission_classes = [IsAuthenticated,IsMechanicPermission]
+###########
 class VisitDescriptionViewSet(viewsets.ModelViewSet):
     queryset = VisitDescription.objects.all()
     serializer_class = VisitDescriptionSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
 
+
+class VisitDescriptionClientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = VisitDescription.objects.all()
+    serializer_class = VisitDescriptionSerializer
+    permission_classes = [IsAuthenticated,IsMechanicPermission]
+
+
+########
 class VisitViewSet(viewsets.ModelViewSet):
     queryset = Visit.objects.all()
     serializer_class = VisitSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
     
+class VisitClientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Visit.objects.all()
+    serializer_class = VisitSerializer
+    permission_classes = [IsAuthenticated,IsMechanicPermission]
+    
+
+#####
 class CarPlaceViewSet(viewsets.ModelViewSet):
     queryset = CarPlace.objects.all()
     serializer_class = CarPlaceSerializer
     permission_classes = [IsAuthenticated,IsMechanicPermission]
 
+class CarPlaceClientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CarPlace.objects.all()
+    serializer_class = CarPlaceSerializer
+    permission_classes = [IsAuthenticated,IsMechanicPermission]
 
-class YourReparingCars(APIView):
+#####
+class YourReparingCars(viewsets.GenericViewSet,mixins.ListModelMixin):
+    """
+    description:
+        Return a list of reparings
+    """
 
-    def get(self,request,format=None):
-        """
-        description:
-            Return a list of reparings
-            Length list is equal to "count" parameter
-            Repair: id_visit(can be null), list[id_services],cartaken(true/false)
-        :description
-        query:
-            - count<->required<->
-            - page<->notrequired<->
-        :query
-        """
-        user = request.user
+    serializer_class = RepairSerializer
+    permission_classes = [IsAuthenticated]
+
+
+    def get_queryset(self):
+        user = self.request.user
 
         repairs = Repair.objects.filter(
             visit__car__owner__user=user
         )
-        repairs_serialized = RepairSerializer(repairs,many=True)
 
-        print(user.userinfo)
+        return repairs
 
-
-        return Response(repairs_serialized.data)
-
-    schema = MyOwnSchema(description=get.__doc__)
 
 
